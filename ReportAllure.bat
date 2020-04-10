40a0ec50d5d4e4c9b79786639513867e2dafb75d
Aleksandr Kuzhelev <alexkuzh@gmail.com>
2020-04-10 17:07:50 -0400

    update

---
+++ ReportAllure.bat
@@ -0,0 +1,4 @@
+cd D:
+cd D:\Documents\GitHub\Selenium\ReportsA
+py.test --alluredir=D:/Documents/GitHub/Selenium/ReportsA/AllureReports ./UnitTest_Allure_report.py
+allure serve D:/Documents/GitHub/Selenium/ReportsA/AllureReports
---
+++ ReportHtml.bat
@@ -0,0 +1,3 @@
+cd d:
+cd D:\Documents\GitHub\Selenium\ReportsH
+python UnitTest_html_report.py
